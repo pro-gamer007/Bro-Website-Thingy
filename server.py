0538219a497b6daa460e9916d2a07b3fb30db98e
pro_gamer007 <coolaryan@icloud.com>
2020-12-04 22:15:06 +0530

    last err hopefully unless i think i fucked everything up lel

--- server.py
+++ server.py
@@ -91,7 +91,34 @@ def switch1_On():
 
 @app.route('/ontwo/')
 def switch2_On():
-      print("Turning Switch2 ON" + "\n")
+  # set the pins numbering mode
+    GPIO.setmode(GPIO.BOARD)
+
+    # Select the GPIO pins used for the encoder K0-K3 data inputs
+    GPIO.setup(11, GPIO.OUT)
+    GPIO.setup(15, GPIO.OUT)
+    GPIO.setup(16, GPIO.OUT)
+    GPIO.setup(13, GPIO.OUT)
+
+    # Select the signal to select ASK/FSK
+    GPIO.setup(18, GPIO.OUT)
+
+    # Select the signal used to enable/disable the modulator
+    GPIO.setup(22, GPIO.OUT)
+
+    # Disable the modulator by setting CE pin lo
+    GPIO.output (22, False)
+
+    # Set the modulator to ASK for On Off Keying 
+    # by setting MODSEL pin lo
+    GPIO.output (18, False)
+
+    # Initialise K0-K3 inputs of the encoder to 0000
+    GPIO.output (11, False)
+    GPIO.output (15, False)
+    GPIO.output (16, False)
+    GPIO.output (13, False)
+print("Turning Switch2 ON" + "\n")
     # Set socket encoder pins to 1111
 GPIO.output(11, True)
 GPIO.output(15, True)
@@ -103,6 +130,33 @@ time.sleep(0.25)
 GPIO.output(22, False) # Disable Modulator
 @app.route('/offtwo/')
 def switch1_Off():
+  # set the pins numbering mode
+    GPIO.setmode(GPIO.BOARD)
+
+    # Select the GPIO pins used for the encoder K0-K3 data inputs
+    GPIO.setup(11, GPIO.OUT)
+    GPIO.setup(15, GPIO.OUT)
+    GPIO.setup(16, GPIO.OUT)
+    GPIO.setup(13, GPIO.OUT)
+
+    # Select the signal to select ASK/FSK
+    GPIO.setup(18, GPIO.OUT)
+
+    # Select the signal used to enable/disable the modulator
+    GPIO.setup(22, GPIO.OUT)
+
+    # Disable the modulator by setting CE pin lo
+    GPIO.output (22, False)
+
+    # Set the modulator to ASK for On Off Keying 
+    # by setting MODSEL pin lo
+    GPIO.output (18, False)
+
+    # Initialise K0-K3 inputs of the encoder to 0000
+    GPIO.output (11, False)
+    GPIO.output (15, False)
+    GPIO.output (16, False)
+    GPIO.output (13, False)
     print("Turning Switch OFF" + "\n")
     # Set socket encoder pins to 0110
     GPIO.output(11, False)
