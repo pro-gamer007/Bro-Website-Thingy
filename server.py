1c63cef2233bc85867713040c076080f8ae16fd7
pro_gamer007 <coolaryan@icloud.com>
2020-12-04 15:59:31 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,6 +2,35 @@ from flask import Flask, render_template
 import RPi.GPIO as GPIO
 app = Flask(__name__)
 ## stuff idk yet lmaoo
+def init_pins():
+    # set the pins numbering mode
+    GPIO.setmode(GPIO.BOARD)
+
+    # Select the GPIO pins used for the encoder K0-K3 data inputs
+    GPIO.setup(11, GPIO.OUT)
+    GPIO.setup(15, GPIO.OUT)
+    GPIO.setup(16, GPIO.OUT)
+    GPIO.setup(13, GPIO.OUT)
+
+    # Select the signal to select ASK/FSK
+    GPIO.setup(18, GPIO.OUT)
+
+    # Select the signal used to enable/disable the modulator
+    GPIO.setup(22, GPIO.OUT)
+
+    # Disable the modulator by setting CE pin lo
+    GPIO.output (22, False)
+
+    # Set the modulator to ASK for On Off Keying 
+    # by setting MODSEL pin lo
+    GPIO.output (18, False)
+
+    # Initialise K0-K3 inputs of the encoder to 0000
+    GPIO.output (11, False)
+    GPIO.output (15, False)
+    GPIO.output (16, False)
+    GPIO.output (13, False)
+
 @app.route('/')
 def index():
   return render_template('index.html')
@@ -9,6 +38,18 @@ def index():
 @app.route('/my-link/')
 def my_link():
     print('YOY I GOT CLICKED')
+    # do the shit lmfao aka turn on
+    def switch1_On():
+    # Set socket encoder pins to 0111
+    GPIO.output(11, False)
+    GPIO.output(15, True)
+    GPIO.output(16, True)
+    GPIO.output(13, True)
+    time.sleep(0.1)
+    GPIO.output(22, True) # Enable Modulator
+    time.sleep(0.25)
+    GPIO.output(22, False) # Disable Modulator
+
     return 'Turned on?'
 if __name__ == '__main__':
     app.run(debug=False)
